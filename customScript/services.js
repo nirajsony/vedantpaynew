b99cd93214247cb70a2b0159e90fc3ffb318b30e
Niraj Soni <nirajsony321@yahoo.com>
2021-07-13 13:36:11 +0530

    First Commit

---
+++ customScript/services.js
@@ -0,0 +1,56 @@
+$(document).ready((e) => {
+  AOS.init({
+    duration: 1000,
+    offset: 0,
+  }
+  );
+  // Script for Service Page starts here
+  $('.autoplay').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+
+  // Script for Service Page ends here
+  $(document).ready(function () {
+    console.log($(window).scrollTop());
+  });
+});
+
+
+
+
+
+
