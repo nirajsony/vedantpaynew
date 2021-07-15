8ef688b1a81c351fd2afced62b7caa578bac0211
Niraj Soni <nirajsony321@yahoo.com>
2021-07-15 10:34:47 +0530

    blank template

--- js/customScript/custom.js
+++ js/customScript/custom.js
@@ -14,13 +14,13 @@
     autoplaySpeed: 2000,
     arrows:false,
     dots: false,
+    infinite: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
           slidesToScroll: 1,
-          infinite: true,
         }
       },
       {
